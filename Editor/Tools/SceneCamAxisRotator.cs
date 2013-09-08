11eca898557da6d8fa191816d70b75c64ba3e003
David Dionisio <garapata@gmail.com>
2013-09-07 18:06:29 -0700

    scene camera axis rotator to make life easier for gitgirl editing

---
+++ Editor/Tools/SceneCamAxisRotator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SceneCamAxisRotator : EditorWindow {
+    private float r = 0.0f;
+
+    [MenuItem("M8/Tools/Camera Axis Rotator")]
+    static void DoIt() {
+        EditorWindow.GetWindow(typeof(SceneCamAxisRotator));
+    }
+    
+    void OnGUI() {
+        GUILayout.BeginHorizontal();
+
+        float prevR = r;
+
+        if(GUILayout.Button("R")) {
+            r = 0.0f;
+        }
+
+        r = EditorGUILayout.Slider("Z-Axis", r, 0, 360);
+        if(prevR != r) {
+            SceneView.currentDrawingSceneView.rotation = Quaternion.AngleAxis(r, Vector3.forward);
+            SceneView.currentDrawingSceneView.Repaint();
+        }
+
+        GUILayout.EndHorizontal();
+    }
+}
