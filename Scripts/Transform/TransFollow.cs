f283002fa19bf87222b512084091ebb709948575
David Dionisio <garapata@gmail.com>
2013-12-26 15:21:01 -0800

    added TransFollow.cs

---
+++ Scripts/Transform/TransFollow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("M8/Transform/Follow")]
+public class TransFollow : MonoBehaviour {
+    public Transform target;
+    public Vector3 ofs;
+
+    public bool lockX;
+    public bool lockY;
+    public bool lockZ;
+
+    public float delay;
+
+    private Vector3 mCurVel;
+
+	void OnEnable() {
+        mCurVel = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update() {
+        if(target) {
+            Vector3 pos = transform.position;
+            Vector3 tgt = target.position + target.rotation*ofs;
+            if(lockX)
+                tgt.x = pos.x;
+            if(lockY)
+                tgt.y = pos.y;
+            if(lockZ)
+                tgt.z = pos.z;
+
+            transform.position = Vector3.SmoothDamp(pos, tgt, ref mCurVel, delay, Mathf.Infinity, Time.deltaTime);
+        }
+	}
+
+    void OnDrawGizmosSelected() {
+        if(target) {
+            Gizmos.color = Color.green*0.5f;
+            Vector3 pt = target.position + target.rotation*ofs;
+            Gizmos.DrawSphere(pt, 0.1f);
+        }
+    }
+}
