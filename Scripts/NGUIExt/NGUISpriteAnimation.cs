f23d5689f59d8975600e6e5ef4d46c820c0abf65
David Dionisio <garapata@gmail.com>
2013-12-27 15:22:08 -0800

    added NGUIFillAnim.cs, added SpriteClipSmoothFill.cs (not implemented)

--- Scripts/NGUIExt/NGUISpriteAnimation.cs
+++ Scripts/NGUIExt/NGUISpriteAnimation.cs
@@ -8,35 +8,69 @@ public class NGUISpriteAnimation : MonoBehaviour {
     public string[] frames;
     public float framesPerSecond;
 
+    public bool resetOnStop = false;
+    public bool autoPlay = true;
     public bool makePixelPerfect = false;
 
     private int mCurFrame;
     private float mFrameCounter;
+    private bool mStarted;
+    private bool mPlaying;
+
+    public void Play() {
+        mPlaying = true;
+    }
+
+    public void Stop() {
+        if(resetOnStop) {
+            mCurFrame = 0;
+            mFrameCounter = 0;
+            SetToCurrentFrame();
+        }
+
+        mPlaying = false;
+    }
 
     void Awake() {
         if(frameHolder == null)
             frameHolder = GetComponent<UISprite>();
     }
 
+    void OnEnable() {
+        if(mStarted && autoPlay)
+            Play();
+    }
+
+    void OnDisable() {
+        Stop();
+    }
+
     // Use this for initialization
     void Start() {
-        mFrameCounter = 0;
+        mCurFrame = 0;
         mFrameCounter = 0;
         SetToCurrentFrame();
+
+        mStarted = true;
+
+        if(autoPlay)
+            Play();
     }
 
     // Update is called once per frame
     void Update() {
-        mFrameCounter += Time.deltaTime * framesPerSecond;
-        int newFrame = Mathf.RoundToInt(mFrameCounter);
-        if(mCurFrame != newFrame) {
-            mCurFrame = newFrame;
-            if(mCurFrame >= frames.Length) {
-                mCurFrame = 0;
-                mFrameCounter -= (float)frames.Length;
-            }
+        if(mPlaying) {
+            mFrameCounter += Time.deltaTime * framesPerSecond;
+            int newFrame = Mathf.RoundToInt(mFrameCounter);
+            if(mCurFrame != newFrame) {
+                mCurFrame = newFrame;
+                if(mCurFrame >= frames.Length) {
+                    mCurFrame = 0;
+                    mFrameCounter -= (float)frames.Length;
+                }
 
-            SetToCurrentFrame();
+                SetToCurrentFrame();
+            }
         }
     }
 
---
+++ Scripts/tk2dExt/SpriteClipSmoothFill.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("M8/tk2D/SpriteClipSmoothFill")]
+public class SpriteClipSmoothFill : MonoBehaviour {
+    public enum Mode {
+        Once,
+        Repeat,
+        PingPong
+    }
+
+    public enum Type {
+        X,
+        Y,
+        Both
+    }
+
+    public Mode mode = Mode.Once;
+    public Type type = Type.X;
+    public float start = 0.0f;
+    public float end = 1.0f;
+    public float delay = 1.0f;
+    public bool playOnEnable = false;
+
+    private bool mStarted;
+
+    void OnEnable() {
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
