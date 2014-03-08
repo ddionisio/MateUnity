9c67161cf97c28da61003129a856b82751f7896b
David Dionisio <garapata@gmail.com>
2014-03-07 23:08:21 -0800

    added TransRotateTowards.cs

---
+++ Scripts/Transform/TransRotateTowards.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("M8/Transform/RotateTowards")]
+public class TransRotateTowards : MonoBehaviour {
+    public Transform target;
+
+    public float speed;
+
+    public bool local;
+
+    // Update is called once per frame
+    void Update() {
+        if(target) {
+            if(local)
+                transform.localRotation = Quaternion.RotateTowards(transform.localRotation, target.localRotation, speed*Time.deltaTime);
+            else
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, speed*Time.deltaTime);
+        }
+    }
+}
