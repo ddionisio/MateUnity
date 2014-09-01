2c5a47f910d9be2e7d5be46e5aca7c4077a87e39
David Dionisio <garapata@gmail.com>
2014-08-31 18:19:39 -0700

    added SpriteColorOverride.cs for changing multiple sprites with one color

---
+++ Editor/2D/SpriteColorOverrideInspector.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(SpriteColorOverride))]
+public class SpriteColorOverrideInspector : Editor {
+    /*SpriteRenderer[] _targets;
+
+    [SerializeField]
+    bool _recursive = false;
+
+    [SerializeField]
+    bool _includeInactive = false;
+
+    [SerializeField]
+    Color _color;*/
+
+    private SerializedProperty mTargets;
+    private SerializedProperty mRecursive;
+    private SerializedProperty mIncludeInactive;
+    private SerializedProperty mColor;
+
+    void OnEnable() {
+        mTargets = serializedObject.FindProperty("_targets");
+        mRecursive = serializedObject.FindProperty("_recursive");
+        mIncludeInactive = serializedObject.FindProperty("_includeInactive");
+        mColor = serializedObject.FindProperty("_color");
+    }
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        if(GUI.changed) {
+            Color clr = mColor.colorValue;
+
+            if(mTargets.arraySize > 0) {
+                for(int i = 0; i < mTargets.arraySize; i++) {
+                    SpriteRenderer render = mTargets.GetArrayElementAtIndex(i).objectReferenceValue as SpriteRenderer;
+                    render.color = clr;
+                }
+            }
+            else {
+                bool recursive = mRecursive.boolValue;
+                bool includeInactive = mIncludeInactive.boolValue;
+
+                if(recursive) {
+                    SpriteRenderer[] renders = (target as MonoBehaviour).GetComponentsInChildren<SpriteRenderer>(includeInactive);
+                    for(int i = 0; i < renders.Length; i++) {
+                        if(renders[i]) renders[i].color = clr;
+                    }
+                }
+                else {
+                    SpriteRenderer render = (target as MonoBehaviour).GetComponent<SpriteRenderer>();
+                    if(render) render.color = clr;
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/2D/SpriteColorOverride.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("M8/Sprite/ColorOverride")]
+public class SpriteColorOverride : MonoBehaviour {
+    [SerializeField]
+    SpriteRenderer[] _targets;
+
+    [SerializeField]
+    bool _recursive = false;
+
+    [SerializeField]
+    bool _includeInactive = false;
+
+    [SerializeField]
+    Color _color;
+
+    private SpriteRenderer[] mSpriteRenders;
+
+    public Color color {
+        get { return _color; }
+        set {
+            if(_color != value) {
+                _color = value;
+                for(int i = 0; i < mSpriteRenders.Length; i++) {
+                    if(mSpriteRenders[i]) mSpriteRenders[i].color = _color;
+                }
+            }
+        }
+    }
+
+    void Awake() {
+        if(_targets == null || _targets.Length == 0) {
+            if(_recursive) {
+                mSpriteRenders = GetComponentsInChildren<SpriteRenderer>(_includeInactive);
+            }
+            else {
+                mSpriteRenders = new SpriteRenderer[1];
+                mSpriteRenders[0] = GetComponent<SpriteRenderer>();
+            }
+        }
+        else
+            mSpriteRenders = _targets;
+
+        color = _color;
+    }
+}
