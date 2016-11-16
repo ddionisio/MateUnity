4c0c14e75a024f97cdd3ffa657b67af31a041e0c
ddionisio <garapata@gmail.com>
2016-11-15 23:19:59 -0800

    Added a very basic stats API so I don't have to keep making the same thing every project. May end up collecting dust after one project.

---
+++ Editor/Game/StatsControllerInspector.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace M8 {
+    [CustomEditor(typeof(StatsController))]
+    public class StatsControllerInspector : Editor {
+
+        private bool mIsEditMode;
+        private List<int> mCheckedIds;
+
+        private Vector2 mScroll;
+
+        public override void OnInspectorGUI() {
+            var statsCtrl = (StatsController)target;
+
+            var stats = serializedObject.FindProperty("_stats");
+            var statsItems = stats.FindPropertyRelative("_statItems");
+
+            //Items
+            GUILayout.BeginVertical();
+
+            if(Application.isPlaying) {
+                mIsEditMode = false;
+                                
+                foreach(var stat in statsCtrl) {
+                    //grab name
+                    string name;
+
+                    int statDataIndex = StatsTemplateConfig.GetStatsIndex(stat.id);
+                    if(statDataIndex != -1)
+                        name = StatsTemplateConfig.stats[statDataIndex].name;
+                    else
+                        name = "<Unknown> ID: "+stat.id;
+
+                    string valStr = string.Format("{0}/{1}", stat.currentValue, stat.value);
+
+                    GUILayout.BeginHorizontal(GUI.skin.box);
+
+                    EditorGUILayout.LabelField(name, valStr);
+
+                    GUILayout.EndHorizontal();
+                }
+            }
+            else {
+                if(mIsEditMode) {
+                    foreach(var statData in StatsTemplateConfig.stats) {
+                        int idIndex = mCheckedIds.IndexOf(statData.id);
+
+                        GUILayout.BeginHorizontal(GUI.skin.box);
+
+                        bool check = GUILayout.Toggle(idIndex != -1, statData.name);
+                        if(check) {
+                            if(idIndex == -1)
+                                mCheckedIds.Add(statData.id);
+                        }
+                        else {
+                            if(idIndex != -1)
+                                mCheckedIds.RemoveAt(idIndex);
+                        }
+
+                        GUILayout.Space(10f);
+
+                        GUILayout.Label("ID: "+statData.id.ToString(), GUILayout.Width(40f));
+
+                        GUILayout.EndHorizontal();
+                    }
+                }
+                else {
+                    for(int i = 0; i < statsItems.arraySize; i++) {
+                        var idValue = statsItems.GetArrayElementAtIndex(i).FindPropertyRelative("_id").intValue;
+
+                        var val = statsItems.GetArrayElementAtIndex(i).FindPropertyRelative("_value");
+                        var clamp = statsItems.GetArrayElementAtIndex(i).FindPropertyRelative("_clamp");
+                        
+                        //grab name
+                        string name;
+
+                        int statDataIndex = StatsTemplateConfig.GetStatsIndex(idValue);
+                        if(statDataIndex != -1)
+                            name = StatsTemplateConfig.stats[statDataIndex].name;
+                        else
+                            name = "<Unknown> ID: "+idValue;
+
+                        GUILayout.BeginHorizontal(GUI.skin.box);
+                        
+                        EditorGUILayout.PropertyField(val, new GUIContent(name));
+
+                        GUILayout.FlexibleSpace();
+                        
+                        EditorGUILayout.PropertyField(clamp, new GUIContent("Clamped"));
+
+                        GUILayout.EndHorizontal();
+                    }
+
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            GUILayout.EndVertical();
+
+            var lastEnabled = GUI.enabled;
+            var lastBkgrndClr = GUI.backgroundColor;
+
+            GUILayout.Space(8f);
+             
+            //Settings
+            if(mIsEditMode) {
+                GUILayout.BeginHorizontal();
+
+                //Save
+                GUI.backgroundColor = Color.green;
+                if(GUILayout.Button("Save")) {
+                    mIsEditMode = false;
+
+                    //go through and restructure stat items
+                    var newStats = new List<StatItem>();
+
+                    foreach(var statData in StatsTemplateConfig.stats) {
+                        if(mCheckedIds.Contains(statData.id)) {
+                            //grab existing stat item
+                            var statItem = statsCtrl[statData.id];
+                            if(statItem == null)
+                                statItem = new StatItem(statData.id);
+
+                            newStats.Add(statItem);
+                        }
+                    }
+
+                    Undo.RecordObject(target, "Save Stat Settings For " + target.name);
+
+                    statsCtrl.Override(newStats.ToArray());
+                }
+
+                GUILayout.Space(16f);
+
+                //Cancel
+                GUI.backgroundColor = Color.red;
+                if(GUILayout.Button("Cancel")) {
+                    mIsEditMode = false;
+                }
+
+                GUILayout.EndHorizontal();
+            }
+            else {
+                GUI.enabled = lastEnabled && !Application.isPlaying;
+                
+                if(GUILayout.Button("Settings")) {
+                    mIsEditMode = true;
+
+                    //setup checked list
+                    mCheckedIds = new List<int>();
+
+                    foreach(var stat in statsCtrl)
+                        mCheckedIds.Add(stat.id);
+                }
+            }
+
+
+            GUI.enabled = lastEnabled;
+            GUI.backgroundColor = lastBkgrndClr;
+            
+            if(GUILayout.Button("Edit Template"))
+                StatsTemplateConfig.Open();
+        }
+    }
+}
---
+++ Scripts/Game/StatsController.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace M8 {
+    [System.Serializable]
+    public class StatItem {
+        public const int InvalidID = 0;
+        
+        [SerializeField]
+        int _id;
+
+        [SerializeField]
+        float _value;
+
+        [SerializeField]
+        bool _clamp;
+
+        public int id { get { return _id; } }
+
+        public float value { get { return _value; } }
+
+        public float currentValue {
+            get { return mCurVal; }
+            set {
+                if(mCurVal != value) {
+                    var prevVal = mCurVal;
+                    mCurVal = _clamp ? Mathf.Clamp(value, 0f, _value) : value;
+
+                    if(changeCallback != null)
+                        changeCallback(this, mCurVal - prevVal);
+                }
+            }
+        }
+        
+        public event System.Action<StatItem, float> changeCallback; //float delta
+        public event System.Action<StatItem> resetCallback;
+
+        private float mCurVal;
+                
+        public StatItem(int aId) {
+            _id = aId;
+            _value = 0f;
+            _clamp = true;
+        }
+
+        public void Reset() {
+            mCurVal = _value;
+
+            if(resetCallback != null)
+                resetCallback(this);
+        }
+    }
+
+    [System.Serializable]
+    public class Stats : IEnumerable<StatItem> {
+        [SerializeField]
+        StatItem[] _statItems;
+
+        public Stats(StatItem[] items) {
+            _statItems = items;
+        }
+        
+        public StatItem GetStatItem(int id) {
+            for(int i = 0; i < _statItems.Length; i++) {
+                if(_statItems[i].id == id)
+                    return _statItems[i];
+            }
+            return null;
+        }
+
+        public void Reset() {
+            for(int i = 0; i < _statItems.Length; i++)
+                _statItems[i].Reset();
+        }
+                
+        public IEnumerator<StatItem> GetEnumerator() {
+            for(int i = 0; i < _statItems.Length; i++)
+                yield return _statItems[i];
+        }
+                
+        IEnumerator IEnumerable.GetEnumerator() {
+            yield return GetEnumerator();
+        }
+
+        //TODO: database of sort
+    }
+
+    /// <summary>
+    /// Simple way for a dynamic stats
+    /// </summary>
+    [AddComponentMenu("M8/Game/Stats")]
+    public class StatsController : MonoBehaviour, IEnumerable<StatItem> {
+        [SerializeField]
+        Stats _stats;
+
+        public StatItem this[int id] {
+            get {
+                if(_stats == null)
+                    return null;
+
+                return _stats.GetStatItem(id);
+            }
+        }
+
+        public IEnumerator<StatItem> GetEnumerator() {
+            return ((IEnumerable<StatItem>)_stats).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable<StatItem>)_stats).GetEnumerator();
+        }
+
+        public void Reset() {
+            _stats.Reset();
+        }
+
+        public void Override(StatItem[] items) {
+            _stats = new Stats(items);
+        }
+        
+        void Awake() {
+            Reset();
+        }
+    }
+}
