edf87581ec6dc4018e0e83f7b11803de90a3eb47
ddionisio <garapata@gmail.com>
2017-01-05 02:02:39 -0800

    added SceneLoad and OpenModal for conveniences

---
+++ Scripts/Core/SceneLoad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace M8 {
+    /// <summary>
+    /// Simple behaviour to load a scene via Execute, useful for UI, or some timeline editor
+    /// </summary>
+    [AddComponentMenu("M8/Core/SceneLoad")]
+    public class SceneLoad : MonoBehaviour {
+        [SerializeField]
+        SceneAssetPath _scene;
+        
+        public void Execute() {
+            SceneManager.instance.LoadScene(_scene.name);
+        }
+	}
+}
---
+++ Scripts/UIModal/Helpers/OpenModal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8.UI.Modal.Helpers {
+    /// <summary>
+    /// Simple behaviour to open a modal via Execute, useful for UI, or some timeline editor
+    /// </summary>
+    [AddComponentMenu("M8/UI Modal/Controller/OpenModal")]
+    public class OpenModal : MonoBehaviour {
+        public string modalRef;
+        public bool closeIfOpened;
+
+        public void Execute() {
+            if(UIModal.Manager.instance.isBusy)
+                return;
+
+            if(UIModal.Manager.instance.ModalIsInStack(modalRef)) {
+                if(closeIfOpened)
+                    UIModal.Manager.instance.ModalCloseUpTo(modalRef, true);
+            }
+            else
+                UIModal.Manager.instance.ModalOpen(modalRef);
+        }
+    }
+}
