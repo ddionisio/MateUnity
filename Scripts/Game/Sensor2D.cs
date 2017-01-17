e9d5ed8ad3c574763fb233c1bfa03cce92dacfbe
ddionisio <garapata@gmail.com>
2017-01-16 23:43:55 -0800

    added Sensor2D

---
+++ Scripts/Game/Sensor2D.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace M8 {
+    public abstract class Sensor2D<T> : MonoBehaviour where T : Component {
+        protected abstract bool UnitVerify(T unit);
+        protected abstract void UnitAdded(T unit);
+        protected abstract void UnitRemoved(T unit);
+
+        protected Collider2D mCollider;
+
+        private HashSet<T> mUnits = new HashSet<T>();
+
+        public HashSet<T> items {
+            get {
+                return mUnits;
+            }
+        }
+
+        public void CleanUp() {
+            mUnits.RemoveWhere(IsUnitInvalid);
+        }
+
+        protected virtual void OnEnable() {
+            mCollider.enabled = true;
+        }
+
+        protected virtual void OnDisable() {
+            mCollider.enabled = false;
+        }
+
+        protected virtual void Awake() {
+            mCollider = GetComponent<Collider2D>();
+        }
+
+        void OnTriggerEnter2D(Collider2D other) {
+            CleanUp();
+            T unit = other.GetComponent<T>();
+            if(unit != null && UnitVerify(unit)) {
+                if(mUnits.Add(unit)) {
+                    UnitAdded(unit);
+                }
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other) {
+            CleanUp();
+            T unit = other.GetComponent<T>();
+            if(unit != null && mUnits.Remove(unit)) {
+                UnitRemoved(unit);
+            }
+        }
+
+        bool IsUnitInvalid(T unit) {
+            if(unit != null) {
+                if(!unit.gameObject.activeInHierarchy || !UnitVerify(unit)) {
+                    UnitRemoved(unit);
+                    return true;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
