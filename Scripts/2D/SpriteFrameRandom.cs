af682702504e7f313b961279d28bb029a1bf5b14
ddionisio <garapata@gmail.com>
2017-02-03 20:35:53 -0800

    added SpriteFrameRandom

---
+++ Scripts/2D/SpriteFrameRandom.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    [AddComponentMenu("M8/Sprite/FrameRandom")]
+    public class SpriteFrameRandom : MonoBehaviour {
+        public SpriteRenderer sprite;
+
+        public Sprite[] frames;
+
+        public float startDelay; //pause at start in seconds
+        public float nextDelayMin = 0.4f; //time it takes to go to next frame
+        public float nextDelayMax = 1.0f; //time it takes to go to next frame
+
+        public bool playOnStart;
+
+        private bool mActive;
+        private Coroutine mRout;
+
+        public void Play() {
+            mActive = true;
+            if(mRout == null)
+                mRout = StartCoroutine(DoPlay());
+        }
+
+        public void Stop() {
+            mActive = false;
+
+            if(mRout != null) {
+                StopCoroutine(mRout);
+                mRout = null;
+            }
+        }
+
+        void OnEnable() {
+            if(mActive)
+                Play();
+        }
+
+        void OnDisable() {
+            if(mRout != null) {
+                StopCoroutine(mRout);
+                mRout = null;
+            }
+        }
+
+        void Awake() {
+            if(sprite == null) {
+                sprite = GetComponent<SpriteRenderer>();
+            }
+        }
+
+        void Start() {
+            if(playOnStart)
+                Play();
+        }
+
+        IEnumerator DoPlay() {
+            yield return new WaitForSeconds(startDelay);
+
+            while(true) {
+                sprite.sprite = frames[Random.Range(0, frames.Length)];
+                yield return new WaitForSeconds(Random.Range(nextDelayMin, nextDelayMax));
+            }
+        }
+    }
+}
