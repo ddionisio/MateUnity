39353b79b393031ea81e1d62b7cde7b0b018a52d
ddionisio <garapata@gmail.com>
2017-02-23 01:58:04 -0800

    added custom localize loader

--- Editor/Core/LocalizeInspector.cs
+++ Editor/Core/LocalizeInspector.cs
@@ -6,9 +6,43 @@ namespace M8 {
     [CustomEditor(typeof(Localize))]
     public class LocalizeInspector : Editor {
         public override void OnInspectorGUI() {
-            if(GUILayout.Button("Edit")) {
-                LocalizeConfig.Open(target as Localize);
+            var propMode = serializedObject.FindProperty("mode");
+
+            EditorGUILayout.PropertyField(propMode);
+
+            var curMode = (Localize.Mode)propMode.enumValueIndex;
+
+            switch(curMode) {
+                case Localize.Mode.CustomLoader:
+                    EditorGUILayout.PropertyField(serializedObject.FindProperty("loader"));
+                    break;
+                case Localize.Mode.TextAsset:
+                    if(GUILayout.Button("Edit")) {
+                        LocalizeConfig.Open(target as Localize);
+                    }
+                    break;
+
+                default:
+                    break;
             }
+
+            M8.EditorExt.Utility.DrawSeparator();
+
+            //
+            var propTables = serializedObject.FindProperty("tables");
+
+            var prevColor = GUI.backgroundColor;
+            GUI.backgroundColor = Color.red;
+
+            GUI.enabled = propTables.arraySize > 0;
+            if(GUILayout.Button("Clear Table")) {
+                propTables.arraySize = 0;
+            }
+
+            GUI.backgroundColor = prevColor;
+            //
+            
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
---
+++ Scripts/Core/LocalizeLoaderBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    [AddComponentMenu("")]
+    public abstract class LocalizeLoaderBase : MonoBehaviour {
+        public abstract int GetLanguageIndex(string language);
+        public abstract string GetLanguageString(int languageIndex);
+        public abstract int GetLanguageCount();
+
+        /// <summary>
+        /// Populate output based on given language index
+        /// </summary>
+        public abstract void Load(int languageIndex, Dictionary<string, Localize.Data> output);
+    }
+}
