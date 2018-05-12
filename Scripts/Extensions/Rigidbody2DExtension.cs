23a9c1820d76913333489cbf01a3b93aa4b43c41
ddionisio <garapata@gmail.com>
2018-05-12 12:12:04 -0400

    add option to not use "wearoff" in Explode at Rigidbody2DExtension

--- Scripts/Extensions/Rigidbody2DExtension.cs
+++ Scripts/Extensions/Rigidbody2DExtension.cs
@@ -4,26 +4,53 @@ using UnityEngine;
 
 namespace M8 {
     public static class Rigidbody2DExtension {
-        public static void AddExplosionForce(this Rigidbody2D body, float explosionForce, Vector2 explosionPosition, float explosionRadius, ForceMode2D mode) {
+        public static void AddExplosionForce(this Rigidbody2D body, float explosionForce, Vector2 explosionPosition, float explosionRadius, bool applyWearOff, ForceMode2D mode) {
             var delta = (body.position - explosionPosition);
             var dist = delta.magnitude;
-            
-            float wearoff = 1 - (dist / explosionRadius);
-            if(wearoff > 0f) {
-                var dir = delta / dist;
 
-                Vector3 baseForce = dir * (explosionForce * wearoff);
-                body.AddForce(baseForce, mode);
+            if(dist > 0f) {
+                if(applyWearOff) {
+                    float wearoff = 1 - (dist / explosionRadius);
+                    if(wearoff > 0f) {
+                        var dir = delta / dist;
+                        Vector3 baseForce = dir * (explosionForce * wearoff);
+                        body.AddForce(baseForce, mode);
+                    }
+                }
+                else {
+                    var dir = delta / dist;
+                    Vector3 baseForce = dir * explosionForce;
+                    body.AddForce(baseForce, mode);
+                }
             }
         }
 
-        public static void AddExplosionForce(this Rigidbody2D body, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier, ForceMode2D mode) {
+        public static void AddExplosionForce(this Rigidbody2D body, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier, bool applyWearOff, ForceMode2D mode) {
             var delta = (body.position - explosionPosition);
-                        
+
             float explodeDist = delta.magnitude;
-                        
-            float wearoff = 1 - (explodeDist / explosionRadius);
-            if(wearoff > 0f) {
+
+            if(applyWearOff) {
+                float wearoff = 1 - (explodeDist / explosionRadius);
+                if(wearoff > 0f) {
+                    float deltaDist;
+                    if(upwardsModifier != 0f) {
+                        delta.y += upwardsModifier;
+
+                        deltaDist = delta.magnitude;
+                    }
+                    else
+                        deltaDist = explodeDist;
+
+                    if(deltaDist > 0f) {
+                        var dir = delta / deltaDist;
+
+                        Vector3 baseForce = dir * (explosionForce * wearoff);
+                        body.AddForce(baseForce, mode);
+                    }
+                }
+            }
+            else {
                 float deltaDist;
                 if(upwardsModifier != 0f) {
                     delta.y += upwardsModifier;
@@ -33,33 +60,62 @@ namespace M8 {
                 else
                     deltaDist = explodeDist;
 
-                var dir = delta / deltaDist;
+                if(deltaDist > 0f) {
+                    var dir = delta / deltaDist;
 
-                Vector3 baseForce = dir * (explosionForce * wearoff);                                
-                body.AddForce(baseForce, mode);
+                    Vector3 baseForce = dir * explosionForce;
+                    body.AddForce(baseForce, mode);
+                }
             }
         }
 
-        public static void AddExplosionForceAtPosition(this Rigidbody2D body, Vector2 bodyPosition, float explosionForce, Vector2 explosionPosition, float explosionRadius, ForceMode2D mode) {
+        public static void AddExplosionForceAtPosition(this Rigidbody2D body, Vector2 bodyPosition, float explosionForce, Vector2 explosionPosition, float explosionRadius, bool applyWearOff, ForceMode2D mode) {
             var delta = (body.position - explosionPosition);
             var dist = delta.magnitude;
 
-            float wearoff = 1 - (dist / explosionRadius);
-            if(wearoff > 0f) {
-                var dir = delta / dist;
-
-                Vector3 baseForce = dir * (explosionForce * wearoff);
-                body.AddForceAtPosition(baseForce, bodyPosition, mode);
+            if(dist > 0f) {
+                if(applyWearOff) {
+                    float wearoff = 1 - (dist / explosionRadius);
+                    if(wearoff > 0f) {
+                        var dir = delta / dist;
+                        Vector3 baseForce = dir * (explosionForce * wearoff);
+                        body.AddForceAtPosition(baseForce, bodyPosition, mode);
+                    }
+                }
+                else {
+                    var dir = delta / dist;
+                    Vector3 baseForce = dir * explosionForce;
+                    body.AddForceAtPosition(baseForce, bodyPosition, mode);
+                }
             }
         }
 
-        public static void AddExplosionForceAtPosition(this Rigidbody2D body, Vector2 bodyPosition, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier, ForceMode2D mode) {
+        public static void AddExplosionForceAtPosition(this Rigidbody2D body, Vector2 bodyPosition, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier, bool applyWearOff, ForceMode2D mode) {
             var delta = (body.position - explosionPosition);
 
             float explodeDist = delta.magnitude;
 
-            float wearoff = 1 - (explodeDist / explosionRadius);
-            if(wearoff > 0f) {
+            if(applyWearOff) {
+                float wearoff = 1 - (explodeDist / explosionRadius);
+                if(wearoff > 0f) {
+                    float deltaDist;
+                    if(upwardsModifier != 0f) {
+                        delta.y += upwardsModifier;
+
+                        deltaDist = delta.magnitude;
+                    }
+                    else
+                        deltaDist = explodeDist;
+
+                    if(deltaDist > 0f) {
+                        var dir = delta / deltaDist;
+
+                        Vector3 baseForce = dir * (explosionForce * wearoff);
+                        body.AddForceAtPosition(baseForce, bodyPosition, mode);
+                    }
+                }
+            }
+            else {
                 float deltaDist;
                 if(upwardsModifier != 0f) {
                     delta.y += upwardsModifier;
@@ -69,10 +125,12 @@ namespace M8 {
                 else
                     deltaDist = explodeDist;
 
-                var dir = delta / deltaDist;
+                if(deltaDist > 0f) {
+                    var dir = delta / deltaDist;
 
-                Vector3 baseForce = dir * (explosionForce * wearoff);
-                body.AddForceAtPosition(baseForce, bodyPosition, mode);
+                    Vector3 baseForce = dir * explosionForce;
+                    body.AddForceAtPosition(baseForce, bodyPosition, mode);
+                }
             }
         }
     }
