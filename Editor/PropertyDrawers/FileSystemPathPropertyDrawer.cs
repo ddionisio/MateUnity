00950918de4939921ba69a54b7603d40701f3b9c
ddionisio <garapata@gmail.com>
2018-07-11 15:18:54 -0400

    added FileSystemPathAttribute for easy filepath browsing in inspector

---
+++ Editor/PropertyDrawers/FileSystemPathPropertyDrawer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace M8 {
+    [CustomPropertyDrawer(typeof(FileSystemPathAttribute))]
+    public class FileSystemPathPropertyDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginProperty(position, label, property);
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var fileSystemPathAttr = this.attribute as FileSystemPathAttribute;
+
+            const float dialogButtonWidth = 25f;
+            const float space = 4f;
+
+            var textFieldPos = position; textFieldPos.width -= dialogButtonWidth + space;
+            property.stringValue = EditorGUI.TextField(textFieldPos, property.stringValue);
+
+            var dialogButtonPos = new Rect(textFieldPos.max.x + space, textFieldPos.min.y, dialogButtonWidth, position.height);
+            if(GUI.Button(dialogButtonPos, "...")) {
+                var path = property.stringValue;
+                if(string.IsNullOrEmpty(path))
+                    path = Application.dataPath;
+
+                string folder = System.IO.Path.GetDirectoryName(path);
+
+                property.stringValue = EditorUtility.OpenFilePanel(fileSystemPathAttr.fileDialogTitle, folder, fileSystemPathAttr.fileExtension);
+            }
+            
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Scripts/Properties/FileSystemPathAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    /// <summary>
+    /// Inspector helper for grabbing file path from the OS
+    /// </summary>
+    public class FileSystemPathAttribute : PropertyAttribute {
+        public string fileDialogTitle { get; set; }
+        public string fileExtension { get; set; }
+
+        public FileSystemPathAttribute(string fileDialogTitle, string fileExtension) {
+            this.fileDialogTitle = fileDialogTitle;
+            this.fileExtension = fileExtension;
+        }
+    }
+}
