ecc383b135523a6eef706c98d3bf41a7c9239c43
ddionisio <garapata@gmail.com>
2018-08-06 19:31:17 -0400

    added EventClick, expose "Target Raycast" in InvisibleInspector

--- Editor/UI/Widgets/InvisibleInspector.cs
+++ Editor/UI/Widgets/InvisibleInspector.cs
@@ -4,6 +4,13 @@ namespace M8.UI.Widgets {
     [CustomEditor(typeof(Invisible))]
     public class InvisibleInspector : Editor {
         public override void OnInspectorGUI() {
+            var dat = target as Invisible;
+
+            var raycastTarget = EditorGUILayout.Toggle("Raycast Target", dat.raycastTarget);
+            if(dat.raycastTarget != raycastTarget) {
+                Undo.RecordObject(dat, "Change Raycast Target");
+                dat.raycastTarget = raycastTarget;
+            }
         }
     }
 }
---
+++ Scripts/UI/Events/EventClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace M8.UI.Events {
+    [AddComponentMenu("M8/UI/Events/Click")]
+    public class EventClick : MonoBehaviour, IPointerClickHandler {
+        public UnityEvent clickEvent;
+        public Signal clickSignal;
+
+        void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
+            clickEvent.Invoke();
+
+            if(clickSignal)
+                clickSignal.Invoke();
+        }
+    }
+}
