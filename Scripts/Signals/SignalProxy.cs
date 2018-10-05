5369e73f44deba2a79bde7bbb3b4561920d32e82
ddionisio <garapata@gmail.com>
2018-10-05 18:15:57 -0400

    added SignalProxy

---
+++ Scripts/Signals/SignalProxy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    /// <summary>
+    /// Use this to allow calling of Signal.Invoke through hookups
+    /// </summary>
+    [AddComponentMenu("M8/Signals/Proxy")]
+    public class SignalProxy : MonoBehaviour {
+        public Signal signal;
+
+        public void Invoke() {
+            signal.Invoke();
+        }
+    }
+}
