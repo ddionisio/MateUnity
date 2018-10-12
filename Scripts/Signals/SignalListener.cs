0af2d13223dbe19cba2bb55cd04d9fd184685d2a
ddionisio <garapata@gmail.com>
2018-10-12 17:19:31 -0400

    added SignalListener

---
+++ Scripts/Signals/SignalListener.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace M8 {
+    /// <summary>
+    /// Use this to allow calling of Signal.Invoke through hookups
+    /// </summary>
+    [AddComponentMenu("M8/Signals/Listener")]
+    public class SignalListener : MonoBehaviour {
+        public Signal signal;
+
+        [Tooltip("If true, only listen when this behaviour is enabled.")]
+        [SerializeField]
+        bool _activeOnly;
+
+        public UnityEvent onSignal;
+
+        void OnDisable() {
+            if(_activeOnly) {
+                if(signal)
+                    signal.callback -= OnSignal;
+            }
+        }
+
+        void OnEnable() {
+            if(_activeOnly) {
+                if(signal)
+                    signal.callback += OnSignal;
+            }
+        }
+
+        void OnDestroy() {
+            if(!_activeOnly) {
+                if(signal)
+                    signal.callback -= OnSignal;
+            }
+        }
+
+        void Awake() {
+            if(!_activeOnly) {
+                if(signal)
+                    signal.callback += OnSignal;
+            }
+        }
+
+        void OnSignal() {
+            onSignal.Invoke();
+        }
+    }
+}
