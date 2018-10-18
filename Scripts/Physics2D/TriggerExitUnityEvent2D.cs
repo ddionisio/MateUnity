f7b7e8e73fd6b9ff15218397cf2b2ee8426335cc
ddionisio <garapata@gmail.com>
2018-10-18 18:40:23 -0400

    added Trigger(Enter/Exit)UnityEvent2D

---
+++ Scripts/Physics2D/TriggerExitUnityEvent2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace M8 {
+    [AddComponentMenu("M8/Physics2D/Trigger Exit Event")]
+    public class TriggerExitUnityEvent2D : MonoBehaviour {
+        [Tooltip("Which tags is allowed to invoke callback. Set this to empty to allow any collision.")]
+        [TagSelector]
+        public string[] tagFilters;
+
+        public UnityEventCollider2D callback;
+
+        void OnTriggerExit2D(Collider2D collision) {
+            int filterInd = -1;
+            for(int i = 0; i < tagFilters.Length; i++) {
+                var tagFilter = tagFilters[i];
+                if(!string.IsNullOrEmpty(tagFilter) && collision.CompareTag(tagFilter)) {
+                    filterInd = i;
+                    break;
+                }
+            }
+
+            if(filterInd != -1)
+                callback.Invoke(collision);
+        }
+    }
+}
---
+++ Scripts/Physics2D/UnityEventCollider2D.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace M8 {
+    [System.Serializable]
+    public class UnityEventCollider2D : UnityEvent<Collider2D> {
+    }
+}
