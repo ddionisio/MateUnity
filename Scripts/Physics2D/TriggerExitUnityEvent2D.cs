406e00ea7fe7754865526d339c1049c77ba550e2
ddionisio <garapata@gmail.com>
2018-10-24 16:51:26 -0400

    ignore tagFilters check if empty in Trigger(Enter/Exit)UnityEvent2D

--- Scripts/Physics2D/TriggerExitUnityEvent2D.cs
+++ Scripts/Physics2D/TriggerExitUnityEvent2D.cs
@@ -11,16 +11,20 @@ namespace M8 {
         public UnityEventCollider2D callback;
 
         void OnTriggerExit2D(Collider2D collision) {
-            int filterInd = -1;
-            for(int i = 0; i < tagFilters.Length; i++) {
-                var tagFilter = tagFilters[i];
-                if(!string.IsNullOrEmpty(tagFilter) && collision.CompareTag(tagFilter)) {
-                    filterInd = i;
-                    break;
+            if(tagFilters.Length > 0) {
+                int filterInd = -1;
+                for(int i = 0; i < tagFilters.Length; i++) {
+                    var tagFilter = tagFilters[i];
+                    if(!string.IsNullOrEmpty(tagFilter) && collision.CompareTag(tagFilter)) {
+                        filterInd = i;
+                        break;
+                    }
                 }
-            }
 
-            if(filterInd != -1)
+                if(filterInd != -1)
+                    callback.Invoke(collision);
+            }
+            else
                 callback.Invoke(collision);
         }
     }
