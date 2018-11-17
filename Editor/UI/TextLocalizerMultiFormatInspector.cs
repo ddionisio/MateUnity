131f3a423acaef5dff759953ff9da60211642e81
ddionisio <garapata@gmail.com>
2018-11-17 17:18:01 -0500

    added LocalizerMultiFormat

---
+++ Editor/UI/TextLocalizerMultiFormatInspector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace M8.UI {
+    [CustomEditor(typeof(Texts.LocalizerMultiFormat), true)]
+    public class TextLocalizerMultiFormatInspector : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            EditorExt.Utility.DrawSeparator();
+
+            //preview (for now, just base)
+            GUI.enabled = LocalizeEdit.isLocalizeFileExists;
+
+            if(GUILayout.Button("Preview")) {
+                var dat = target as Texts.LocalizerMultiFormat;
+
+                var textUI = dat.GetComponent<UnityEngine.UI.Text>();
+                if(textUI) {
+                    dat.Apply();
+                    EditorUtility.SetDirty(textUI);
+                }
+            }
+
+            GUI.enabled = true;
+        }
+    }
+}
---
+++ Scripts/UI/Texts/LocalizerMultiFormat.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace M8.UI.Texts {
+    [AddComponentMenu("M8/UI/Texts/Localizer Multi Format")]
+    [RequireComponent(typeof(Text))]
+    public class LocalizerMultiFormat : MonoBehaviour {
+        /// <summary>
+        /// Localization key.
+        /// </summary>
+        [Localize]
+        public string[] keys;
+
+        public string format = "{0}";
+
+        public Text uiText { get; private set; }
+
+        private bool mStarted = false;
+        private string[] mTexts;
+
+        /// <summary>
+        /// Localize the widget on enable, but only if it has been started already.
+        /// </summary>
+
+        void OnEnable() { Localize.instance.localizeCallback += Apply; if(mStarted) Apply(); }
+        void OnDisable() { if(Localize.instance) Localize.instance.localizeCallback -= Apply; }
+
+        /// <summary>
+        /// Localize the widget on start.
+        /// </summary>
+
+        void Start() {
+            mStarted = true;
+            Apply();
+        }
+
+        /// <summary>
+        /// Force-localize the widget.
+        /// </summary>
+        public void Apply() {
+            if(uiText == null)
+                uiText = GetComponent<Text>();
+
+            if(mTexts == null || mTexts.Length != keys.Length)
+                mTexts = new string[keys.Length];
+
+            for(int i = 0; i < keys.Length; i++) {
+                var key = keys[i];
+                if(Localize.instance.Exists(key))
+                    mTexts[i] = Localize.Get(key);
+                else
+                    mTexts[i] = "";
+            }
+
+            uiText.text = string.Format(format, mTexts);
+        }
+    }
+}
