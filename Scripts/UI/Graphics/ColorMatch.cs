5c20752cd9d434b81445e5eec90e458ce4f3a83b
ddionisio <garapata@gmail.com>
2018-11-18 19:44:31 -0500

    added UI/Graphics/ColorMatch

---
+++ Scripts/UI/Graphics/ColorMatch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace M8.UI.Graphics {
+    [AddComponentMenu("M8/UI/Graphics/ColorMatch")]
+    [ExecuteInEditMode]
+    public class ColorMatch : MonoBehaviour {
+        public Graphic source;
+        public Graphic target;
+        public Color colorMod = Color.white;
+
+        private Color mCurColor;
+
+        void OnEnable() {
+            if(source != null || target != null) {
+                mCurColor = source.color;
+                target.color = mCurColor * colorMod;
+            }
+        }
+
+        void Update() {
+            if(source == null || target == null)
+                return;
+
+            if(mCurColor != source.color) {
+                mCurColor = source.color;
+                target.color = mCurColor * colorMod;
+            }
+        }
+    }
+}
