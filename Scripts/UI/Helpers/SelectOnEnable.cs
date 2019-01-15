de7029c71fa7699212c0723ad3e2fb1aac7a3ddc
ddionisio <garapata@gmail.com>
2019-01-15 18:52:45 -0500

    added SelectOnEnable for UI

---
+++ Scripts/UI/Helpers/SelectOnEnable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace M8.UI {
+    [AddComponentMenu("M8/UI/Selectable/Select On Enable")]
+    public class SelectOnEnable : MonoBehaviour {
+        public Selectable target;
+
+        void OnEnable() {
+            target.Select();
+        }
+
+        void Awake() {
+            if(!target)
+                target = GetComponent<Selectable>();
+        }
+    }
+}
