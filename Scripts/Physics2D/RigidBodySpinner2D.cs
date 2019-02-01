d9607871c4a85fccd57364268e1ed1939f279d97
ddionisio <garapata@gmail.com>
2019-02-01 10:43:34 -0500

    added RigidBodySpinner2D

---
+++ Scripts/Physics2D/RigidBodySpinner2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    [AddComponentMenu("M8/Physics2D/Spinner")]
+    public class RigidBodySpinner2D : MonoBehaviour {
+
+        public Rigidbody2D target;
+
+        public float rotateSpeed;
+
+        public float scale { get; private set; } = 1.0f;
+
+        void Awake() {
+            if(!target)
+                target = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate() {
+            float rot = target.rotation;
+            float rotDelta = rotateSpeed * Time.fixedDeltaTime * scale;
+            target.MoveRotation(rot + rotDelta);
+        }
+    }
+}
