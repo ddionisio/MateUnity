a340e841503087c6accd9c40d55de1c6c2cae03b
ddionisio <garapata@gmail.com>
2019-03-18 22:26:06 -0400

    added TextVersion

---
+++ Editor/UI/TextVersionInspector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+namespace M8.UI {
+    [CustomEditor(typeof(Texts.TextVersion), true)]
+    public class TextVersionInspector : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            EditorExt.Utility.DrawSeparator();
+            
+            if(GUILayout.Button("Preview")) {
+                var dat = target as Texts.TextVersion;
+                dat.Apply();
+            }
+        }
+    }
+}
---
+++ Scripts/UI/Texts/TextVersion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace M8.UI.Texts {
+    /// <summary>
+    /// Use this to display the version from Unity project settings.
+    /// </summary>
+    [AddComponentMenu("M8/UI/Texts/Version")]
+    public class TextVersion : MonoBehaviour {
+        public Text target;
+
+        public void Apply() {
+            if(!target)
+                target = GetComponent<Text>();
+
+            if(target)
+                target.text = Application.version;
+        }
+
+        void Awake() {
+            Apply();
+        }
+    }
+}
