5a382819febf869f002fe474701e885d8077496a
ddionisio <garapata@gmail.com>
2019-03-25 11:44:59 -0400

    added ModalInputAction

---
+++ Scripts/Modal/Helpers/ModalInputAction.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace M8 {
+    /// <summary>
+    /// Use this to allow callback for pressed and released, mostly for button type actions.
+    /// </summary>
+    [AddComponentMenu("M8/Modal/Helpers/Input Action")]
+    public class ModalInputAction : MonoBehaviour, IModalActive {
+        [System.Serializable]
+        public struct Data {
+            public InputAction action;
+            public UnityEventInputAction callback;
+
+            public InputAction.ButtonState curState { get { return mCurState; } }
+
+            private InputAction.ButtonState mCurState;
+
+            public void Reset() {
+                mCurState = InputAction.ButtonState.None;
+            }
+
+            public void Update() {
+                var newState = action.GetButtonState();
+
+                //state has changed
+                if(mCurState != newState) {
+                    mCurState = newState;
+
+                    switch(mCurState) {
+                        case InputAction.ButtonState.Pressed:
+                            if(callback != null)
+                                callback.Invoke(action);
+                            break;
+                    }
+                }
+            }
+        }
+
+        public Data[] actions;
+
+        private bool mIsActive;
+
+        void IModalActive.SetActive(bool aActive) {
+            mIsActive = aActive;
+            if(!mIsActive) {
+                for(int i = 0; i < actions.Length; i++)
+                    actions[i].Reset();
+            }
+        }
+
+        void OnDisable() {
+            for(int i = 0; i < actions.Length; i++)
+                actions[i].Reset();
+        }
+
+        void Update() {
+            if(SceneManager.isInstantiated && SceneManager.instance.isLoading)
+                return;
+
+            for(int i = 0; i < actions.Length; i++)
+                actions[i].Update();
+        }
+    }
+}
