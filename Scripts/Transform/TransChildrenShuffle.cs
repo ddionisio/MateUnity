c6c7eec0bf0433a300de59f806294cf0c13d8412
David Dionisio <ddionisio@renegadeware.com>
2019-07-27 14:59:27 -0400

    added Transform/TransChildrenShuffle.cs

---
+++ Scripts/Transform/TransChildrenShuffle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    [AddComponentMenu("M8/Transform/ChildrenShuffle")]
+    public class TransChildrenShuffle : MonoBehaviour {
+        public enum Mode {
+            None,
+            Awake,
+            Start,
+            Enable
+        }
+
+        public Mode mode = Mode.Enable;
+        public Transform target; //if null, use self
+
+        public void Shuffle() {
+            var t = target ? target : transform;
+
+            var count = t.childCount;
+
+            for(int i = 0; i < count; i++) {
+                int r = Random.Range(i, count);
+
+                var child1 = t.GetChild(i);
+                var child2 = t.GetChild(r);
+
+                child1.SetSiblingIndex(r);
+                child2.SetSiblingIndex(i);
+            }
+        }
+
+        void OnEnable() {
+            if(mode == Mode.Enable)
+                Shuffle();
+        }
+
+        void Awake() {
+            if(mode == Mode.Awake)
+                Shuffle();
+        }
+
+        void Start() {
+            if(mode == Mode.Start)
+                Shuffle();
+        }
+    }
+}
