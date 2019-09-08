b59d4fa988732bcf9f2ce876194b4d30dcb3c8f4
David Dionisio <ddionisio@renegadeware.com>
2019-09-08 15:45:07 -0400

    added SpriteRandom, added SpritesDepthEnabled.shader

---
+++ Scripts/2D/SpriteRandom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("M8/Sprite/Random")]
+public class SpriteRandom : MonoBehaviour {
+    public SpriteRenderer spriteRender;
+
+    public Sprite[] sprites;
+
+    public void Apply() {
+        if(spriteRender)
+            spriteRender.sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+    void OnEnable() {
+        if(spriteRender == null)
+            spriteRender = GetComponent<SpriteRenderer>();
+
+        Apply();
+    }
+}
