9bd39658dbe09c9d2117cd4318132202cf0e6c2f
David Dionisio <ddionisio@renegadeware.com>
2020-08-05 17:07:14 -0400

    added more feature to ColorGradientTextureGenerator (curve/step), properly generate import settings

--- Editor/Color/ColorGradientTextureGeneratorInspector.cs
+++ Editor/Color/ColorGradientTextureGeneratorInspector.cs
@@ -7,15 +7,43 @@ using System.IO;
 namespace M8 {
     [CustomEditor(typeof(ColorGradientTextureGenerator))]
     public class ColorGradientTextureGeneratorInspector : Editor {
+        private SerializedProperty mModeProp;
+
+        private SerializedProperty mMonoProp;
+
+        private SerializedProperty mClrBeginProp;
+        private SerializedProperty mClrEndProp;
+
         private SerializedProperty mGradientProp;
+
+        private SerializedProperty mCurveProp;
+
+        private SerializedProperty mStepsProp;
+
         private SerializedProperty mTextureWidthProp;
+        private SerializedProperty mTextureFilterProp;
+        private SerializedProperty mTextureWrapProp;
+
+        private SerializedProperty mTextureProp;
 
         private GUIContent[] mTextureWidthLabels;
         private int[] mTextureWidths;
 
         void OnEnable() {
+            mModeProp = serializedObject.FindProperty("_mode");
+
+            mMonoProp = serializedObject.FindProperty("_mono");
+
+            mClrBeginProp = serializedObject.FindProperty("_colorBegin");
+            mClrEndProp = serializedObject.FindProperty("_colorEnd");
+
             mGradientProp = serializedObject.FindProperty("_gradient");
-            mTextureWidthProp = serializedObject.FindProperty("_textureWidth");
+
+            mCurveProp = serializedObject.FindProperty("_curve");
+
+            mStepsProp = serializedObject.FindProperty("_steps");
+
+            mTextureWidthProp = serializedObject.FindProperty("_textureWidth");            
 
             mTextureWidthLabels = new GUIContent[13];
             mTextureWidths = new int[13];
@@ -24,6 +52,10 @@ namespace M8 {
                 mTextureWidthLabels[i] = new GUIContent(val.ToString());
                 mTextureWidths[i] = val;
             }
+
+            mTextureFilterProp = serializedObject.FindProperty("_textureFilterMode");
+            mTextureWrapProp = serializedObject.FindProperty("_textureWrapMode");
+            mTextureProp = serializedObject.FindProperty("_textureTarget");
         }
 
         /*private string GetFilename(string path) {
@@ -39,43 +71,136 @@ namespace M8 {
         }*/
 
         private string GetTextureFilePath() {
-            var objName = target.name;
-            var objPath = AssetDatabase.GetAssetPath(target);
-
             string path;
 
-            int extInd = objPath.LastIndexOf('.');
-            if(extInd != -1)
-                path = objPath.Substring(0, extInd);
-            else
-                path = objPath.Substring(0, objPath.Length);
+            if(mTextureProp.objectReferenceValue) {
+                path = AssetDatabase.GetAssetPath(mTextureProp.objectReferenceValue);
+            }
+            else {
+                //var objName = target.name;
+                var objPath = AssetDatabase.GetAssetPath(target);
 
-            path += "_texture.png";
+                int extInd = objPath.LastIndexOf('.');
+                if(extInd != -1)
+                    path = objPath.Substring(0, extInd);
+                else
+                    path = objPath.Substring(0, objPath.Length);
+
+                path += "_texture.png";
+            }
 
             return path;
+        }
+
+        private string GetTextureDir() {
+            string path;
+
+            if(mTextureProp.objectReferenceValue)
+                path = AssetDatabase.GetAssetPath(mTextureProp.objectReferenceValue);
+            else
+                path = AssetDatabase.GetAssetPath(target);
+
+            int extInd = path.LastIndexOf('/');
+            if(extInd == -1)
+                return "Assets";
+
+            return path.Substring(0, extInd);
         }
 
         public override void OnInspectorGUI() {
             serializedObject.Update();
 
-            EditorGUILayout.PropertyField(mGradientProp);
+            //Mode
+            EditorGUILayout.PropertyField(mModeProp);
 
-            EditorGUILayout.IntPopup(mTextureWidthProp, mTextureWidthLabels, mTextureWidths);
+            var mode = (ColorGradientTextureGenerator.Mode)mModeProp.enumValueIndex;
+            var isMono = mode != ColorGradientTextureGenerator.Mode.Gradient && mMonoProp.boolValue;
+
+            //Mode Settings            
+            switch(mode) {
+                case ColorGradientTextureGenerator.Mode.Gradient:
+                    EditorGUILayout.PropertyField(mGradientProp);
+                    break;
+
+                case ColorGradientTextureGenerator.Mode.Curve:
+                    EditorGUILayout.PropertyField(mCurveProp);
+                    break;
+
+                case ColorGradientTextureGenerator.Mode.Steps:
+                    EditorGUILayout.IntSlider(mStepsProp, 1, 256);
+                    break;
+            }
+
+            //Mode Shared Settings
+            switch(mode) {
+                case ColorGradientTextureGenerator.Mode.Curve:
+                case ColorGradientTextureGenerator.Mode.Steps:
+                    EditorGUILayout.PropertyField(mMonoProp);
+
+                    if(!mMonoProp.boolValue) {
+                        EditorGUILayout.PropertyField(mClrBeginProp);
+                        EditorGUILayout.PropertyField(mClrEndProp);
+                    }
+                    break;
+            }
+
+            //Texture Properties
+            if(mode != ColorGradientTextureGenerator.Mode.Steps)
+                EditorGUILayout.IntPopup(mTextureWidthProp, mTextureWidthLabels, mTextureWidths);
+
+            EditorGUILayout.PropertyField(mTextureWrapProp);
+            EditorGUILayout.PropertyField(mTextureFilterProp);            
+
+            EditorExt.Utility.DrawSeparator();
+
+            EditorGUILayout.PropertyField(mTextureProp, new GUIContent("Output"));
 
             serializedObject.ApplyModifiedProperties();
 
-            if(GUILayout.Button("Generate")) {
+            //Texture Generate
+            string savePath = null;
+
+            if(GUILayout.Button("Save"))
+                savePath = GetTextureFilePath();
+
+            if(GUILayout.Button("Save As...")) {
+                var defaultName = mTextureProp.objectReferenceValue ? mTextureProp.objectReferenceValue.name : target.name + "_texture";
+
+                savePath = EditorUtility.SaveFilePanel("Save Texture", GetTextureDir(), defaultName, "png");
+
+                int assetInd = savePath.IndexOf("Assets", System.StringComparison.Ordinal);
+                if(assetInd != -1)
+                    savePath = savePath.Remove(0, assetInd);
+            }
+
+            if(!string.IsNullOrEmpty(savePath)) {
                 var obj = target as ColorGradientTextureGenerator;
 
                 var tex = obj.Generate();
 
                 var texDat = tex.EncodeToPNG();
                 if(texDat != null) {
-                    var textureFilePath = GetTextureFilePath();
-
-                    File.WriteAllBytes(textureFilePath, texDat);
-
+                    File.WriteAllBytes(savePath, texDat);
                     AssetDatabase.Refresh();
+
+                    var texImport = (TextureImporter)AssetImporter.GetAtPath(savePath);
+                    if(texImport != null) {
+                        texImport.wrapMode = tex.wrapMode;
+                        texImport.filterMode = tex.filterMode;
+                        texImport.textureType = isMono ? TextureImporterType.SingleChannel : TextureImporterType.Default;
+                        texImport.alphaIsTransparency = !isMono;                        
+                        texImport.textureCompression = TextureImporterCompression.Uncompressed;
+                        texImport.mipmapEnabled = false;
+                        var textureSettings = new TextureImporterPlatformSettings {
+                            format = isMono ? TextureImporterFormat.R8 : TextureImporterFormat.Automatic
+                        };
+                        texImport.SetPlatformTextureSettings(textureSettings);
+                        EditorUtility.SetDirty(texImport);
+                        texImport.SaveAndReimport();
+
+                        mTextureProp.objectReferenceValue = AssetDatabase.LoadAssetAtPath(texImport.assetPath, typeof(Texture2D));
+                        serializedObject.ApplyModifiedProperties();
+                    }
                 }
 
                 DestroyImmediate(tex);
--- Scripts/Color/ColorGradientTextureGenerator.cs
+++ Scripts/Color/ColorGradientTextureGenerator.cs
@@ -5,18 +5,93 @@ using UnityEngine;
 namespace M8 {
     [CreateAssetMenu(fileName = "gradient", menuName = "M8/Color Gradient")]
     public class ColorGradientTextureGenerator : ScriptableObject {
+        public enum Mode {
+            Gradient,
+            Curve,
+            Steps,
+        }
+
+        public Texture texture { get { return _textureTarget; } }
+
+        [SerializeField]
+        Mode _mode = Mode.Gradient;
+
+        //For curve and step
+        [SerializeField]
+        bool _mono = false; //if true, texture is saved with only one channel
+
+        [SerializeField]
+        Color _colorBegin = Color.black;
+        [SerializeField]
+        Color _colorEnd = Color.white;
+
+        //Gradient
         [SerializeField]
         Gradient _gradient = new Gradient();
+
+        //Curve
+        [SerializeField]
+        AnimationCurve _curve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(1f, 1f));
+
+        //Steps
+        [SerializeField]
+        int _steps = 4;
+                
         [SerializeField]
         int _textureWidth = 256;
+        [SerializeField]
+        FilterMode _textureFilterMode = FilterMode.Bilinear;
+        [SerializeField]
+        TextureWrapMode _textureWrapMode = TextureWrapMode.Clamp;
+
+        [SerializeField]
+        Texture2D _textureTarget = null;
 
         public Texture2D Generate() {
-            var tex = new Texture2D(_textureWidth, 1);
+            int textureWidth = _mode == Mode.Steps ? _steps + 1 : _textureWidth; //texture width for steps is based on count
+            TextureFormat textureFormat = _mode != Mode.Gradient && _mono ? TextureFormat.R8 : TextureFormat.RGBA32; //gradient is strictly 4 channels
 
-            tex.wrapMode = TextureWrapMode.Clamp;
+            var tex = new Texture2D(textureWidth, 1, textureFormat, false);
 
-            for(int i = 0; i < _textureWidth; i++)
-                tex.SetPixel(i, 0, _gradient.Evaluate((float)i / _textureWidth));
+            tex.filterMode = _textureFilterMode;
+            tex.wrapMode = _textureWrapMode;
+
+            switch(_mode) {
+                case Mode.Gradient:
+                    for(int i = 0; i < _textureWidth; i++)
+                        tex.SetPixel(i, 0, _gradient.Evaluate((float)i / _textureWidth));
+                    break;
+
+                case Mode.Curve:
+                    for(int i = 0; i < _textureWidth; i++) {
+                        float t = Mathf.Clamp01(_curve.Evaluate((float)i / textureWidth));
+
+                        Color clr;
+
+                        if(_mono)
+                            clr = new Color(t, t, t);
+                        else
+                            clr = Color.Lerp(_colorBegin, _colorEnd, t);
+
+                        tex.SetPixel(i, 0, clr);
+                    }
+                    break;
+
+                case Mode.Steps:
+                    for(int i = 0; i < textureWidth; i++) {
+                        float t = (float)i / textureWidth;
+
+                        Color clr;
+
+                        if(_mono)
+                            clr = new Color(t, t, t);
+                        else
+                            clr = Color.Lerp(_colorBegin, _colorEnd, t);
+
+                        tex.SetPixel(i, 0, clr);
+                    }
+                    break;
+            }
 
             tex.Apply();
 
