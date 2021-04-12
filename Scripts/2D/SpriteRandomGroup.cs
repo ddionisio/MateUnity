3d6c1eb82e66f646170fc5e48ce42e83157578c2
David Dionisio <ddionisio@renegadeware.com>
2021-04-12 18:49:31 -0400

    added SpriteRandomGroup.cs

---
+++ Scripts/2D/SpriteRandomGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace M8 {
+    [AddComponentMenu("M8/Sprite/Random Group")]
+    public class SpriteRandomGroup : MonoBehaviour {
+        public SpriteRenderer[] spriteRenders;
+
+        public Sprite[] sprites;
+
+        public void Apply() {
+            var spr = sprites[Random.Range(0, sprites.Length)];
+
+            for(int i = 0; i < spriteRenders.Length; i++)
+                spriteRenders[i].sprite = spr;
+        }
+
+        void OnEnable() {
+            Apply();
+        }
+
+        void Awake() {
+            if(spriteRenders == null || spriteRenders.Length == 0)
+                spriteRenders = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+}
