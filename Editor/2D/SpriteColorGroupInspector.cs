ca039392dcb55210534ea1524e0d760f2c8c007d
David Dionisio <ddionisio@renegadeware.com>
2021-04-25 00:24:12 -0400

    added "Revert Colors" in inspector of SpriteColorGroup

---
+++ Editor/2D/SpriteColorGroupInspector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace M8 {
+    [CustomEditor(typeof(SpriteColorGroup))]
+    public class SpriteColorGroupInspector : Editor {
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            EditorExt.Utility.DrawSeparator();
+
+            var dat = target as SpriteColorGroup;
+
+            if(GUILayout.Button("Revert")) {
+                dat.Revert();
+            }
+        }
+    }
+}
