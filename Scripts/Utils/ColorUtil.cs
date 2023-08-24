62766f6f753765cfa91d29a2d71cd363217f6d6f
David Dionisio <ddionisio@renegadeware.com>
2023-08-24 18:23:53 -0400

    added extra params for ColorUtil.Lerp for specific array range

--- Scripts/Utils/ColorUtil.cs
+++ Scripts/Utils/ColorUtil.cs
@@ -25,6 +25,31 @@ namespace M8 {
             }
         }
 
+        public static Color Lerp(Color[] colors, int start, int count, float t) {
+            if(count == 1)
+                return colors[start];
+
+            int endOfs = count - 1;
+
+            if(t >= 1.0f) {
+                return colors[start + endOfs];
+            }
+            else if(t <= 0.0f) {
+                return colors[start];
+            }
+            else {
+                float fIndOfs = t * endOfs;
+                int indOfs = Mathf.FloorToInt(fIndOfs);
+
+                int ind = start + indOfs;
+
+                if(indOfs >= endOfs)
+                    return colors[ind];
+
+                return Color.Lerp(colors[ind], colors[ind + 1], fIndOfs - ind);
+            }
+        }
+
         public static Color LerpRepeat(Color[] colors, float t) {
             if(colors.Length == 1)
                 return colors[0];
