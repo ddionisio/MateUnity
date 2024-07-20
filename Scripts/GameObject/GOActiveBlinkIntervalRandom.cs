df0ce35f883f01cd332752bd8a6747ad850ff2d4
David Dionisio <ddionisio@renegadeware.com>
2024-07-20 15:40:12 -0400

    rename GOActiveIntervalRandom to GOActiveBlinkIntervalRandom

--- Scripts/GameObject/GOActiveBlinkIntervalRandom.cs
+++ Scripts/GameObject/GOActiveBlinkIntervalRandom.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace M8 {
-	[AddComponentMenu("M8/Game Object/Active Interval Random")]
-	public class GOActiveIntervalRandom : MonoBehaviour {		
+	[AddComponentMenu("M8/Game Object/Active Blink Interval Random")]
+	public class GOActiveBlinkIntervalRandom : MonoBehaviour {		
 		public GameObject activeGO;
 		public GameObject inactiveGO;
 
@@ -12,7 +12,8 @@ namespace M8 {
 
 		public RangeFloat delayRange;
 
-		public RangeFloat delayBlinkRange;
+		public RangeInt blinkCount;
+		public RangeFloat blinkDelayRange;
 
 		public bool isRealTime;
 
@@ -35,27 +36,35 @@ namespace M8 {
 		void Awake() {
 			if(isRealTime) {
 				mWaitDelay = new WaitForSecondsRealtimeRandom(delayRange.min, delayRange.max);
-				mWaitBlinkDelay = new WaitForSecondsRealtimeRandom(delayBlinkRange.min, delayBlinkRange.max);
+				mWaitBlinkDelay = new WaitForSecondsRealtimeRandom(blinkDelayRange.min, blinkDelayRange.max);
 			}
 			else {
 				mWaitDelay = new WaitForSecondsRandom(delayRange.min, delayRange.max);
-				mWaitBlinkDelay = new WaitForSecondsRandom(delayBlinkRange.min, delayBlinkRange.max);
+				mWaitBlinkDelay = new WaitForSecondsRandom(blinkDelayRange.min, blinkDelayRange.max);
 			}
 		}
 
 		IEnumerator DoInterval() {
 			var _active = activeStart;
 
-			while(true) {
-				if(activeGO) activeGO.SetActive(_active);
-				if(inactiveGO) inactiveGO.SetActive(!_active);
+			if(activeGO) activeGO.SetActive(_active);
+			if(inactiveGO) inactiveGO.SetActive(!_active);
 
+			while(true) {
 				yield return mWaitDelay;
 
-				if(activeGO) activeGO.SetActive(!_active);
-				if(inactiveGO) inactiveGO.SetActive(_active);
+				var count = blinkCount.random;
+				if(count < 0) count = 1;
 
-				yield return mWaitBlinkDelay;
+				for(int i = 0; i < count; i++) {
+					if(activeGO) activeGO.SetActive(!_active);
+					if(inactiveGO) inactiveGO.SetActive(_active);
+
+					yield return mWaitBlinkDelay;
+
+					if(activeGO) activeGO.SetActive(_active);
+					if(inactiveGO) inactiveGO.SetActive(!_active);
+				}
 			}
 		}
 	}
